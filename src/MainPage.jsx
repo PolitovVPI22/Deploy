a186181772cfd73adae3e1df33652caa46698b3e
Politov VPI22 <61359388+PolitovVPI22@users.noreply.github.com>
2020-05-29 13:49:31 +0300

    Initial commit

---
+++ src/MainPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "./Header";
+
+const MainPage = () => (
+  <div>
+    <Nav />
+    <div className="container">
+      <hr />
+      <h4>1. Авторизироваться. </h4>
+      <h4>2. Проверить данные на странице пользователя. </h4>
+    </div>
+  </div>
+);
+
+export default MainPage;
